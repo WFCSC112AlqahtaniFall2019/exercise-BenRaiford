6d6634fb0873b4d9c647af094b33f8627241e353
benjaminraiford <benjaminhraiford@gmail.com>
2019-09-05 15:57:50 -0400

    changed to vector and changed i into an unsigned long

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 int main() {
-    const int NUM_ELEMENTS = 8;   // Number of elements
-    int revVector[8];               // User values
-    int i;                        // Loop index
+    vector <int> revVector(8); // User values
+    unsigned long i;                        // Loop index
 
 
-    cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
-    for (i = 0; i < NUM_ELEMENTS; i++) {
-        cin >> revVector[i];
+    cout << "Enter " << revVector.size() << " integer values..." << endl;
+    for (i = 0; i < revVector.size(); i++) {
+        cin >> revVector.at(i);
     }
 
     // Reverse
-    for (i = 0; i < NUM_ELEMENTS / 2; i++) {// Swap
-        int tempVar = revVector[i];
-        revVector[i] = revVector[NUM_ELEMENTS - 1 - i];
-        revVector[NUM_ELEMENTS - 1 - i] = tempVar;
+    for (i = 0; i < revVector.size() / 2; i++) {// Swap
+        int tempVar = revVector.at(i);
+        revVector.at(i) = revVector.at(revVector.size() - 1 - i);
+        revVector[revVector.size() - 1 - i] = tempVar;
     }
 
     // Print values
     cout << endl << "New values: ";
-    for (i = 0; i < NUM_ELEMENTS; i++) {
-        cout << " " << revVector[i];
+    for (i = 0; i < revVector.size(); i++) {
+        cout << " " << revVector.at(i);
     }
     cout << endl;
 
