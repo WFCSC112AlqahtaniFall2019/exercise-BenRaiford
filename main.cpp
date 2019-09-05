21155d850d2491debecfad008520b309364a7f06
benjaminraiford <benjaminhraiford@gmail.com>
2019-09-05 15:19:38 -0400

    fixed reverse function

--- main.cpp
+++ main.cpp
@@ -3,23 +3,26 @@ using namespace std;
 
 int main() {
     const int NUM_ELEMENTS = 8;   // Number of elements
-    int revVctr[8];               // User values
+    int revVector[8];               // User values
     int i;                        // Loop index
 
+
     cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
-    for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cin >> revVctr[i];
+    for (i = 0; i < NUM_ELEMENTS; i++) {
+        cin >> revVector[i];
     }
 
     // Reverse
-    for (i = 0; i < NUM_ELEMENTS; ++i) {
-        revVctr[i] = revVctr[NUM_ELEMENTS - i]; // Swap
+    for (i = 0; i < NUM_ELEMENTS / 2; i++) {// Swap
+        int tempVar = revVector[i];
+        revVector[i] = revVector[NUM_ELEMENTS - 1 - i];
+        revVector[NUM_ELEMENTS - 1 - i] = tempVar;
     }
 
     // Print values
     cout << endl << "New values: ";
-    for (i = 0; i < NUM_ELEMENTS; ++i) {
-        cout << " " << revVctr[i];
+    for (i = 0; i < NUM_ELEMENTS; i++) {
+        cout << " " << revVector[i];
     }
     cout << endl;
 
